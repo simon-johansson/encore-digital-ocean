88c4228027d06fbaeaabb9ccc190a8ef1ab16df9
Simon Johansson <simon@encore.dev>
2025-01-08 21:11:15 +0100

    test

---
+++ trigger-railway-deploy.sh
@@ -0,0 +1,5 @@
+curl --request POST \
+  --url https://backboard.railway.com/graphql/v2 \
+  --header 'Authorization: Bearer 3e392dba-86be-465d-82b1-cffc9a0bf04d' \
+  --header 'Content-Type: application/json' \
+  --data '{"query":"query { me { name email } }"}'
